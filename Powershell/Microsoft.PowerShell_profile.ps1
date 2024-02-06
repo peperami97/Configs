8946b67cc450a1f4c410f469eb2d601558661580
Ben Whyall <ben.whyall@cgi.com>
2024-02-06 12:07:40 +0000

    Updates and tidy ups

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+function MtacX{
+   $env:DISPLAY = '127.0.0.1:0'
+   ssh -Y ben@mtac.home
+}
+
+#Invoke-Expression (&starship init powershell)
+oh-my-posh init pwsh --config "~/.myposh.omp.json" | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
